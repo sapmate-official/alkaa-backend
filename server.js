58980d59317a88cc8686cd36ac6ff790c16f31a2
Parambrata Ghosh <gpampa138@gmail.com>
2025-01-10 23:23:45 +0530

    add initial server setup, Prisma schema, and .gitignore

---
+++ server.js
@@ -0,0 +1,59 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import { corsOptions } from './src/util/corsOptions.js'
+import { fileURLToPath } from "url";
+import path from "path";
+import { configDotenv } from 'dotenv';
+import fs from 'fs'
+import https from 'https'
+
+configDotenv()
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors(corsOptions))
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+app.use('/', express.static(path.join(dirname, 'public')))
+
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl}`);
+    next();
+  });
+
+
+
+  import manager from './src/router/manager.router.js'
+  import employee from './src/router/employee.router.js'
+  import general from './src/router/general.router.js'
+  import leave from './src/router/leave.router.js'
+  import attendance from './src/router/attendance.router.js'
+  import salary from './src/router/salary.router.js'
+
+app.use("/api/v1/manager/",manager)
+app.use("/api/v1/employee/",employee)
+app.use("/api/v1/general/",general)
+app.use("/api/v1/leave/",leave)
+app.use("/api/v1/attendance/",attendance)
+app.use("/api/v1/salary/",salary)
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(dirname, 'public', 'interface.html'))
+})
+const options = {
+    key: fs.readFileSync("localhost-key.pem"),
+    cert: fs.readFileSync("localhost.pem"),
+  };
+// app.listen(port,"192.168.0.193", () => {
+//     console.log(`Server is running on port ${port}`)
+// })
+https.createServer(options, app).listen(3001, () => {
+    console.log("Server running on https://192.168.0.193:3001");
+  });
